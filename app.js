c01a32982d1d6fd2ec0e9511dbd6fc8576d11bfc
Dave Strus <dave@getfretless.com>
2017-06-29 16:13:54 -0400

    Remove flicks from the array

--- app.js
+++ app.js
@@ -10,9 +10,14 @@ const app = {
       .addEventListener('submit', this.handleSubmit.bind(this))
   },
 
-  removeFlick(ev) {
+  removeFlick(flick, ev) {
+    // remove from the DOM
     const listItem = ev.target.closest('.flick')
     listItem.remove()
+
+    // remove from the array
+    const i = this.flicks.indexOf(flick)
+    this.flicks.splice(i, 1)
   },
 
   renderListItem(flick) {
@@ -25,7 +30,10 @@ const app = {
 
     item
       .querySelector('button.remove')
-      .addEventListener('click', this.removeFlick)
+      .addEventListener(
+        'click',
+        this.removeFlick.bind(this, flick)
+      )
 
     return item
   },
